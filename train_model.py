9dd8162e9512dd15515fddb8d0c932a807912d56
Jay Gondaliya <139247541+jayg2309@users.noreply.github.com>
2025-04-10 23:56:14 +0530

    Added code for accuracy measurement

--- train_model.py
+++ train_model.py
@@ -10,7 +10,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
 # prepare data
-input_dir = 'C:/Users/jaygo/OneDrive/Desktop/Jay Stuff/Parking-Detection/clf-data'
+input_dir = 'C:/Users/jaygo/OneDrive/Desktop/Jay Stuff/Parking-Detection/clf-data/clf-data'
 categories = ['empty', 'not_empty']
 
 data = []
@@ -48,3 +48,27 @@ score = accuracy_score(y_prediction, y_test)
 print('{}% of samples were correctly classified'.format(str(score * 100)))
 
 pickle.dump(best_estimator, open('./model.p', 'wb'))
+
+# Code for getting the accuracy based on C and gamma parameters
+
+# Modify the parameters grid
+parameters = [{'gamma': [0.01, 0.001, 0.0001], 
+              'C': [1, 10, 100, 1000]}]
+
+grid_search = GridSearchCV(classifier, parameters, cv=5, verbose=2)
+grid_search.fit(x_train, y_train)
+
+# Print detailed accuracy results
+print("\nDetailed Parameter Accuracy Results:")
+print("=====================================")
+means = grid_search.cv_results_['mean_test_score']
+params = grid_search.cv_results_['params']
+
+for mean_score, param in zip(means, params):
+    print(f"\nC: {param['C']}")
+    print(f"gamma: {param['gamma']}")
+    print(f"Accuracy: {mean_score*100:.2f}%")
+
+print("\nBest Parameters:", grid_search.best_params_)
+print(f"Best Cross-validation Accuracy: {grid_search.best_score_*100:.2f}%")
+print(f"Test Set Accuracy: {accuracy_score(y_test, y_prediction)*100:.2f}%")
